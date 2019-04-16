1c516f16d8df96422a8a920e2a273b582cb7a124
Vo Thanh Phuong <vtphuong214@gmail.com>
2019-04-16 12:46:13 +0700

    + update generate speech dir

--- source/constants.py
+++ source/constants.py
@@ -8,3 +8,5 @@ Created on Tue Apr 16 00:38:39 2019
 SAMPLE_RATE = 16000
 WIN_LEN = 0.025
 WIN_STEP = 0.01
+
+ROOT_FOLDER = 'E:/Learning/HCMUS/2018-2019/Khoa Luan Tot Nghiep/Data/VoxCeleb1/wav/'
--- source/generate_speech_direct.py
+++ source/generate_speech_direct.py
@@ -8,6 +8,7 @@ Created on Tue Apr 16 02:36:54 2019
 import os
 import numpy as np
 import shutil
+import constants
 
 def get_all_file(direct, fileType = 'wav'):
     result = []
@@ -22,28 +23,29 @@ def get_all_file(direct, fileType = 'wav'):
     
     return result
 
-def generate_speech_path(root):    
-    # vox1_dev_wav and vox1_test_wav are contained by root
+def generate_speech_path(wav_folder):    
+    #root contains 1251 speakers folder
     
     part = ['dev', 'test']
-    
+     
     for i in range(len(part)):
-        wav_folder = root + 'vox1_{}_wav/wav/'.format(part[i])
-        txt_folder = '../data/vox1_{}_txt/'.format(part[i])
+         
+        txt_folder = '../data/txt/{}_speakers/'.format(part[i])
+        
+        all_speakers_dir = '../data/txt/{}_speakers.txt'.format(part[i]) 
+        all_speakers = np.loadtxt(all_speakers_dir, dtype='str')
         
         # create folder for train/test
         if os.path.isdir(txt_folder):
             shutil.rmtree(txt_folder)
         os.mkdir(txt_folder)
         
-        all_speakers = os.listdir(wav_folder)
-        
         for j in range(len(all_speakers)):
             speaker_folder = wav_folder + all_speakers[j]
             all_files = get_all_file(speaker_folder)
             
             for k in range(len(all_files)):
-                all_files[k] = '/' + all_files[k].replace(root, '')
+                all_files[k] = all_files[k].replace(wav_folder, '')
             
             f_name = txt_folder + all_speakers[j] + '.txt'
             np.savetxt(f_name, all_files, fmt='%s')
@@ -52,6 +54,25 @@ def generate_speech_path(root):
         
     return
 
+
+def get_list_speaker(root):
+    # vox1_dev_wav and vox1_test_wav are contained by root
+    
+    wav_folder = root + 'vox1_test_wav/wav/'
+    test_speakers = os.listdir(wav_folder)
+    np.savetxt('../txt/data/test_speakers.txt', test_speakers, fmt='%s')
+    
+    wav_folder = root + 'vox1_dev_wav/wav/'
+    dev_speakers = os.listdir(wav_folder)
+    np.savetxt('../txt/data/dev_speakers.txt', dev_speakers, fmt='%s')
+    return
+
+
 if __name__ == "__main__":
-    root = 'E:/Learning/HCMUS/2018-2019/Khoa Luan Tot Nghiep/Data/VoxCeleb1/'
-    generate_speech_path(root)
+    # root = 'E:/Learning/HCMUS/2018-2019/Khoa Luan Tot Nghiep/Data/VoxCeleb1/'
+    # get_list_speaker(root)
+    
+    # After call get_list_speaker, join two folders vox1_dev_wav and vox1_test_wav
+    # ROOT_FOLDER contains 1251 folders
+    generate_speech_path(constants.ROOT_FOLDER)
+    
